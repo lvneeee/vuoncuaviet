ad95242c3fe92f9b6ba6b12b8a39af69affabcf1
Le Viet <levietnee@gmail.com>
2025-04-29 03:36:33 +0700

    Quartz sync: Apr 29, 2025, 3:36 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Vườn của Việt 🪴",
+    pageTitle: "Vuon cua Viet🪴",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
